18a307b5491b9b2232ecf53ea9159714f1405e25
shter <shterebandrew@gmail.com>
2023-09-15 13:28:31 +0300

    hw01

---
+++ src/Hwo1.java
@@ -0,0 +1,36 @@
+import java.util.Scanner;
+
+public class Hwo1 {
+
+    public static void main(String[] args) {
+
+        System.out.println("Hello World");
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("say your name");
+
+        String name = scanner.nextLine();
+
+        System.out.println("Hello," + name);
+
+        System.out.println("how old are you?");
+
+        String age = scanner.nextLine();
+
+        System.out.println("where are you from?,");
+
+        String country = scanner.nextLine();
+
+        System.out.println(country + " is a best place in the world");
+
+
+
+
+
+
+
+
+
+    }
+}
